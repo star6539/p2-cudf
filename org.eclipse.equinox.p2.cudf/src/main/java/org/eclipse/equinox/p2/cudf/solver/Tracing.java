d2fb64fbbfa8e24514861abdd34bffb20f09fa01
Pascal Rapicault <prapicau>
2009-12-16 01:20:07 +0000

    add body

--- org.eclipse.equinox.p2.cudf/src/main/java/org/eclipse/equinox/p2/cudf/solver/Tracing.java
+++ org.eclipse.equinox.p2.cudf/src/main/java/org/eclipse/equinox/p2/cudf/solver/Tracing.java
@@ -3,7 +3,7 @@ package org.eclipse.equinox.p2.cudf.solver;
 public class Tracing {
 
 	public static void debug(String string) {
-		// TODO Auto-generated method stub
+		System.out.println(string);
 		
 	}
 
