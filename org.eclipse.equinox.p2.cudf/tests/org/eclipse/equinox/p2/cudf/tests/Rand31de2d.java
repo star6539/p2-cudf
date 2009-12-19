e6e95ed5b0d792f476a8da66e0b771ca83a04524
Pascal Rapicault <prapicau>
2009-12-19 11:50:38 +0000

    fix compile errors

--- org.eclipse.equinox.p2.cudf/tests/org/eclipse/equinox/p2/cudf/tests/Rand31de2d.java
+++ org.eclipse.equinox.p2.cudf/tests/org/eclipse/equinox/p2/cudf/tests/Rand31de2d.java
@@ -1,11 +1,8 @@
 package org.eclipse.equinox.p2.cudf.tests;
 
-import org.eclipse.equinox.p2.cudf.metadata.InstallableUnit;
-
-import java.util.Iterator;
-
 import java.io.File;
 import java.util.Collection;
+import java.util.Iterator;
 import junit.framework.TestCase;
 import org.eclipse.equinox.p2.cudf.Parser;
 import org.eclipse.equinox.p2.cudf.metadata.*;
@@ -14,16 +11,15 @@ import org.eclipse.equinox.p2.cudf.solver.*;
 
 public class Rand31de2d extends TestCase {
 	public void testLibdmx1() {
-		ProfileChangeRequest pcr =  Parser.parse(new File("/Users/pascal/dev/competition/org.eclipse.equinox.p2.cudf/success/rand31de2d-sol.cudf"));
+		ProfileChangeRequest pcr = new Parser().parse(new File("/Users/pascal/dev/competition/org.eclipse.equinox.p2.cudf/success/rand31de2d-sol.cudf"));
 		pcr.addInstallableUnit(new RequiredCapability("libtext-wrapi18n-perl", VersionRange.emptyRange));
 		if (new SimplePlanner().getSolutionFor(pcr) instanceof Collection)
 			return;
 		String id = "libtext-wrapi18n-perl";
 		Version v = new Version(1);
 		QueryableArray res = null;
-		res = slice(pcr.getInitialState(), id,v);
-		restart:
-		for (Iterator iterator = res.iterator(); iterator.hasNext();) {
+		res = slice(pcr.getInitialState(), id, v);
+		restart: for (Iterator iterator = res.iterator(); iterator.hasNext();) {
 			InstallableUnit iu = (InstallableUnit) iterator.next();
 			if (iu.getId() == id)
 				continue;
@@ -39,13 +35,13 @@ public class Rand31de2d extends TestCase {
 			}
 		}
 	}
-	
+
 	private QueryableArray slice(QueryableArray input, String id, Version version) {
-		return new Slicer(input).slice(new InstallableUnit[] { (InstallableUnit) input.query(new CapabilityQuery(new RequiredCapability(id, new VersionRange(version))), new Collector(), null).iterator().next() });
+		return new Slicer(input).slice((InstallableUnit) input.query(new CapabilityQuery(new RequiredCapability(id, new VersionRange(version))), new Collector(), null).iterator().next());
 	}
-	
+
 	public void testValidateAll() {
-		ProfileChangeRequest pcr =  Parser.parse(new File("/Users/pascal/dev/competition/org.eclipse.equinox.p2.cudf/success/rand31de2d-sol.cudf"));
+		ProfileChangeRequest pcr = new Parser().parse(new File("/Users/pascal/dev/competition/org.eclipse.equinox.p2.cudf/success/rand31de2d-sol.cudf"));
 		QueryableArray allIUs = pcr.getInitialState();
 		for (Iterator iterator = allIUs.iterator(); iterator.hasNext();) {
 			InstallableUnit iu = (InstallableUnit) iterator.next();
