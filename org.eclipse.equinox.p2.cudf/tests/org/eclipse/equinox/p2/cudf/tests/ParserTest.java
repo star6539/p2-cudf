546c7f8e154f84034924e3cb6276a2d1f64ccbdc
Pascal Rapicault <prapicau>
2009-12-19 07:02:05 +0000

    *** empty log message ***

--- org.eclipse.equinox.p2.cudf/tests/org/eclipse/equinox/p2/cudf/tests/ParserTest.java
+++ org.eclipse.equinox.p2.cudf/tests/org/eclipse/equinox/p2/cudf/tests/ParserTest.java
@@ -44,6 +44,7 @@ public class ParserTest extends TestCase {
 			}
 		}
 		assertRequirement(new RequiredCapability("f", new VersionRange(new Version(5), true, Version.maxVersion, true)), iu.getRequiredCapabilities());
+		assertRequirement(new NotRequirement(new RequiredCapability("g", VersionRange.emptyRange)), iu.getRequiredCapabilities());
 	}
 
 	public void testCheckPackageLibcbin() {
@@ -55,6 +56,42 @@ public class ParserTest extends TestCase {
 		assertRequirement(new NotRequirement(new RequiredCapability("libc6.1", new VersionRange(Version.emptyVersion, true, new Version(1), false))), iu.getRequiredCapabilities());
 	}
 
+	public void testCheckLibx11data() {
+		InstallableUnit iu = getIU("libx11-data");
+		assertEquals(true, iu.isSingleton());
+	}
+
+	public void testlibtextCharwidthPerl() {
+		InstallableUnit iu = getIU("libtext-charwidth-perl");
+		assertEquals(true, iu.isSingleton());
+		assertRequirement(new RequiredCapability("libc6", new VersionRange(new Version(1), true, Version.maxVersion, true)), iu.getRequiredCapabilities());
+		assertRequirement(new RequiredCapability("perl-base", new VersionRange(new Version(12), true, Version.maxVersion, true)), iu.getRequiredCapabilities());
+		IRequiredCapability[] reqs = iu.getRequiredCapabilities();
+		for (int i = 0; i < reqs.length; i++) {
+			if (reqs[i] instanceof ORRequirement) {
+				assertRequirement(new RequiredCapability("perlapi-5.10.0--virtual", VersionRange.emptyRange), ((ORRequirement) reqs[i]).getRequirements());
+				assertRequirement(new RequiredCapability("perlapi-5.10.0", VersionRange.emptyRange), ((ORRequirement) reqs[i]).getRequirements());
+			}
+		}
+		assertNotRequirement(new NotRequirement(new RequiredCapability("libtext-charwidth-perl", VersionRange.emptyRange)), iu.getRequiredCapabilities());
+	}
+
+	public void testFoo() {
+		InstallableUnit iu = getIU("foo");
+		assertEquals(false, iu.isSingleton());
+		assertProvide(new ProvidedCapability("x", VersionRange.emptyRange), iu.getProvidedCapabilities());
+		assertRequirement(new NotRequirement(new RequiredCapability("x", VersionRange.emptyRange)), iu.getRequiredCapabilities());
+	}
+
+	private void assertNotRequirement(IRequiredCapability asserted, IRequiredCapability[] reqs) {
+		for (int i = 0; i < reqs.length; i++) {
+			if (asserted.getName().equals(reqs[i].getName())) {
+				if (asserted.getRange().equals(reqs[i].getRange()) && asserted.getArity() == reqs[i].getArity() && asserted.isNegation() == reqs[i].isNegation())
+					fail("Requirement not expected:" + asserted);
+			}
+		}
+	}
+
 	private void assertRequirement(IRequiredCapability asserted, IRequiredCapability[] reqs) {
 		boolean found = true;
 		for (int i = 0; i < reqs.length; i++) {
@@ -62,6 +99,16 @@ public class ParserTest extends TestCase {
 				assertEquals(asserted.getRange(), reqs[i].getRange());
 				assertEquals(asserted.getArity(), reqs[i].getArity());
 				assertEquals(asserted.isNegation(), reqs[i].isNegation());
+			}
+		}
+		assertEquals(true, found);
+	}
+
+	private void assertProvide(IProvidedCapability asserted, IProvidedCapability[] caps) {
+		boolean found = true;
+		for (int i = 0; i < caps.length; i++) {
+			if (asserted.getName().equals(caps[i].getName())) {
+				assertEquals(asserted.getVersion(), caps[i].getVersion());
 			}
 		}
 		assertEquals(true, found);
