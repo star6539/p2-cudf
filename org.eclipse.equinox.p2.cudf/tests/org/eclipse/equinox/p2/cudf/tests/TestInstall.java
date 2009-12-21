bb34aa21d6d2daa9fe3961c81c77c60bf83f7e1f
Pascal Rapicault <prapicau>
2009-12-21 10:37:01 +0000

    Fix up copyrights

--- org.eclipse.equinox.p2.cudf/tests/org/eclipse/equinox/p2/cudf/tests/TestInstall.java
+++ org.eclipse.equinox.p2.cudf/tests/org/eclipse/equinox/p2/cudf/tests/TestInstall.java
@@ -1,8 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ * IBM Corporation - initial implementation and ideas 
+ ******************************************************************************/
 package org.eclipse.equinox.p2.cudf.tests;
 
-import junit.framework.TestCase;
-
 import java.util.Collection;
+import junit.framework.TestCase;
 import org.eclipse.equinox.p2.cudf.metadata.*;
 import org.eclipse.equinox.p2.cudf.query.QueryableArray;
 import org.eclipse.equinox.p2.cudf.solver.ProfileChangeRequest;
@@ -14,22 +22,22 @@ public class TestInstall extends TestCase {
 	protected void setUp() throws Exception {
 		InstallableUnit iu = new InstallableUnit();
 		iu.setId("A");
-		iu.setVersion(new Version(1,0,0));
-		iu.setCapabilities(new ProvidedCapability[] {new ProvidedCapability("A", new VersionRange(new Version(1,0,0), true, new Version(1,0,0), true))} );
-		
+		iu.setVersion(new Version(1, 0, 0));
+		iu.setCapabilities(new ProvidedCapability[] {new ProvidedCapability("A", new VersionRange(new Version(1, 0, 0), true, new Version(1, 0, 0), true))});
+
 		InstallableUnit iu2 = new InstallableUnit();
 		iu2.setId("A");
-		iu2.setVersion(new Version(2,0,0));
-		iu2.setCapabilities(new ProvidedCapability[] {new ProvidedCapability("A", new VersionRange(new Version(2,0,0), true, new Version(2,0,0), true))} );
-		
+		iu2.setVersion(new Version(2, 0, 0));
+		iu2.setCapabilities(new ProvidedCapability[] {new ProvidedCapability("A", new VersionRange(new Version(2, 0, 0), true, new Version(2, 0, 0), true))});
+
 		InstallableUnit iu3 = new InstallableUnit();
 		iu3.setId("A");
-		iu3.setVersion(new Version(3,0,0));
-		iu3.setCapabilities(new ProvidedCapability[] {new ProvidedCapability("A", new VersionRange(new Version(3,0,0), true, new Version(3,0,0), true))} );
-		
-		dataSet = new QueryableArray(new InstallableUnit[] { iu,iu2,iu3 });
+		iu3.setVersion(new Version(3, 0, 0));
+		iu3.setCapabilities(new ProvidedCapability[] {new ProvidedCapability("A", new VersionRange(new Version(3, 0, 0), true, new Version(3, 0, 0), true))});
+
+		dataSet = new QueryableArray(new InstallableUnit[] {iu, iu2, iu3});
 	}
-	
+
 	public void testRemoveEverything() {
 		ProfileChangeRequest pcr = new ProfileChangeRequest(dataSet);
 		pcr.addInstallableUnit(new RequiredCapability("A", VersionRange.emptyRange));
