079a7dcbe9da8a6108a2f2b243e8f8f6799e5eba
Daniel LeBerre <dleberre>
2010-11-21 18:04:47 +0000

    Using lexico optimization instead of classical boolean optimization.

--- org.eclipse.equinox.p2.cudf/src/main/java/org/eclipse/equinox/p2/cudf/solver/UserDefinedOptimizationFunction.java
+++ org.eclipse.equinox.p2.cudf/src/main/java/org/eclipse/equinox/p2/cudf/solver/UserDefinedOptimizationFunction.java
@@ -3,6 +3,7 @@ package org.eclipse.equinox.p2.cudf.solver;
 import java.math.BigInteger;
 import java.util.*;
 import org.eclipse.equinox.p2.cudf.metadata.InstallableUnit;
+import org.sat4j.pb.tools.WeightedObject;
 
 public class UserDefinedOptimizationFunction extends OptimizationFunction {
 
@@ -14,43 +15,46 @@ public class UserDefinedOptimizationFunction extends OptimizationFunction {
 
 	public List createOptimizationFunction(InstallableUnit metaIu) {
 		List weightedObjects = new ArrayList();
+		List objects = new ArrayList();
 		BigInteger weight = BigInteger.valueOf(slice.size() + 1);
 		String[] criteria = optfunction.split(",");
 		BigInteger currentWeight = weight.pow(criteria.length - 1);
 		for (int i = 0; i < criteria.length; i++) {
 			if (criteria[i].endsWith("new")) {
+				weightedObjects.clear();
 				niou(weightedObjects, criteria[i].startsWith("+") ? currentWeight.negate() : currentWeight, metaIu);
 				currentWeight = currentWeight.divide(weight);
-				continue;
-			}
-			if (criteria[i].endsWith("removed")) {
+			} else if (criteria[i].endsWith("removed")) {
+				weightedObjects.clear();
 				removed(weightedObjects, criteria[i].startsWith("+") ? currentWeight.negate() : currentWeight, metaIu);
 				currentWeight = currentWeight.divide(weight);
-				continue;
-			}
-			if (criteria[i].endsWith("notuptodate")) {
+			} else if (criteria[i].endsWith("notuptodate")) {
+				weightedObjects.clear();
 				notuptodate(weightedObjects, criteria[i].startsWith("+") ? currentWeight.negate() : currentWeight, metaIu);
 				currentWeight = currentWeight.divide(weight);
-				continue;
-			}
-			if (criteria[i].endsWith("recommended")) {
+			} else if (criteria[i].endsWith("recommended")) {
+				weightedObjects.clear();
 				optional(weightedObjects, criteria[i].startsWith("-") ? currentWeight.negate() : currentWeight, metaIu);
 				currentWeight = currentWeight.divide(weight);
-				continue;
-			}
-			if (criteria[i].endsWith("unmet_recommends")) {
+			} else if (criteria[i].endsWith("unmet_recommends")) {
+				weightedObjects.clear();
 				optional(weightedObjects, criteria[i].startsWith("+") ? currentWeight.negate() : currentWeight, metaIu);
 				currentWeight = currentWeight.divide(weight);
-				continue;
-			}
-			if (criteria[i].endsWith("changed")) {
+			} else if (criteria[i].endsWith("changed")) {
+				weightedObjects.clear();
 				changed(weightedObjects, criteria[i].startsWith("+") ? currentWeight.negate() : currentWeight, metaIu);
 				currentWeight = currentWeight.divide(weight);
-				continue;
+			} else {
+				System.out.println("Skipping unknown criteria:" + criteria[i]);
 			}
-			System.out.println("Skipping unknown criteria:" + criteria[i]);
+			objects.clear();
+			for (Iterator it = weightedObjects.iterator(); it.hasNext();) {
+				objects.add(((WeightedObject) it.next()).thing);
+			}
+			dependencyHelper.addCriterion(objects);
 		}
-		return weightedObjects;
+		weightedObjects.clear();
+		return null;
 	}
 
 	public String getName() {
